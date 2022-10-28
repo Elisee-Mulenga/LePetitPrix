6c513a5fdb217dfa56961fec2afc9088e4314a8c
Elisee_Mulenga <mulengaelisee2@gamil.com>
2022-10-28 09:41:19 +0200

    initial commit

---
+++ js/script.js
@@ -0,0 +1,119 @@
+$function() {
+    $(".en").click(function() {
+        $("nav navbar-nav ml-auto").children(0).eq().text("Home");
+        $("nav navbar-nav ml-auto").children(1).eq().text("About-us");
+        $("nav navbar-nav ml-auto").children(2).eq().text("services");
+        $("nav navbar-nav ml-auto").children(3).eq().text("com-price");
+        $("nav navbar-nav ml-auto").children(4).eq().text("buy");
+        $("nav navbar-nav ml-auto").children(5).eq().text("contact-us");
+        $(".language-selected").text("Langues");
+        $(".language-selected").addclass(".change-en");
+        $(".language-selected").removeclass(".change-br");
+        $(".language-selected").removeclass(".change-sw");
+
+        $(".m-b-20").text("Welcome to Le Petit Prix");
+        $(".m-b-40").text("we are so happy to do good");
+        $(".btn hvr-hover").text("Learn more");
+
+        $(".shop-cat-box").children(0).eq().text("cloc");
+        $(".shop-cat-box").children(1).eq().text("break");
+        $(".shop-cat-box").children(2).eq().text("milk");
+        $(".shop-cat-box").children(3).eq().text("basket");
+        $(".shop-cat-box").children(4).eq().text("legume");
+        $(".shop-cat-box").children(5).eq().text("choes");
+
+        $(".title-all text-center").children(0).eq().text("Article promotion");
+        $(".title-all text-center").children(1).eq().text("the list of truc");
+
+        $(".footer-widget").children(0).eq().text("about agence");
+        $(".footer-widget").children(1).eq().text("lopcsbdhc schdbncs");
+
+        $(".footer-link").children(1).eq().text("Information");
+        $(".footer-link").children(1).eq().text("home");
+        $(".footer-link").children(1).eq().text("about");
+        $(".footer-link").children(1).eq().text("services");
+
+        $(".footer-link-contact").children(1).eq().text("contact");
+
+    });
+}
+
+$function() {
+    $(".en").click(function() {
+        $("nav navbar-nav ml-auto").children(0).eq().text("Home");
+        $("nav navbar-nav ml-auto").children(1).eq().text("About-us");
+        $("nav navbar-nav ml-auto").children(2).eq().text("services");
+        $("nav navbar-nav ml-auto").children(3).eq().text("com-price");
+        $("nav navbar-nav ml-auto").children(4).eq().text("buy");
+        $("nav navbar-nav ml-auto").children(5).eq().text("contact-us");
+        $(".language-selected").text("Langues");
+        $(".language-selected").addclass(".change-en");
+        $(".language-selected").removeclass(".change-br");
+        $(".language-selected").removeclass(".change-sw");
+
+        $(".m-b-20").text("Welcome to Le Petit Prix");
+        $(".m-b-40").text("we are so happy to do good");
+        $(".btn hvr-hover").text("Learn more");
+
+        $(".shop-cat-box").children(0).eq().text("cloc");
+        $(".shop-cat-box").children(1).eq().text("break");
+        $(".shop-cat-box").children(2).eq().text("milk");
+        $(".shop-cat-box").children(3).eq().text("basket");
+        $(".shop-cat-box").children(4).eq().text("legume");
+        $(".shop-cat-box").children(5).eq().text("choes");
+
+        $(".title-all text-center").children(0).eq().text("Article promotion");
+        $(".title-all text-center").children(1).eq().text("the list of truc");
+
+        $(".footer-widget").children(0).eq().text("about agence");
+        $(".footer-widget").children(1).eq().text("lopcsbdhc schdbncs");
+
+        $(".footer-link").children(1).eq().text("Information");
+        $(".footer-link").children(1).eq().text("home");
+        $(".footer-link").children(1).eq().text("about");
+        $(".footer-link").children(1).eq().text("services");
+
+        $(".footer-link-contact").children(1).eq().text("contact");
+
+    });
+}
+
+$function() {
+    $(".en").click(function() {
+        $("nav navbar-nav ml-auto").children(0).eq().text("Home");
+        $("nav navbar-nav ml-auto").children(1).eq().text("About-us");
+        $("nav navbar-nav ml-auto").children(2).eq().text("services");
+        $("nav navbar-nav ml-auto").children(3).eq().text("com-price");
+        $("nav navbar-nav ml-auto").children(4).eq().text("buy");
+        $("nav navbar-nav ml-auto").children(5).eq().text("contact-us");
+        $(".language-selected").text("Langues");
+        $(".language-selected").addclass(".change-en");
+        $(".language-selected").removeclass(".change-br");
+        $(".language-selected").removeclass(".change-sw");
+
+        $(".m-b-20").text("Welcome to Le Petit Prix");
+        $(".m-b-40").text("we are so happy to do good");
+        $(".btn hvr-hover").text("Learn more");
+
+        $(".shop-cat-box").children(0).eq().text("cloc");
+        $(".shop-cat-box").children(1).eq().text("break");
+        $(".shop-cat-box").children(2).eq().text("milk");
+        $(".shop-cat-box").children(3).eq().text("basket");
+        $(".shop-cat-box").children(4).eq().text("legume");
+        $(".shop-cat-box").children(5).eq().text("choes");
+
+        $(".title-all text-center").children(0).eq().text("Article promotion");
+        $(".title-all text-center").children(1).eq().text("the list of truc");
+
+        $(".footer-widget").children(0).eq().text("about agence");
+        $(".footer-widget").children(1).eq().text("lopcsbdhc schdbncs");
+
+        $(".footer-link").children(1).eq().text("Information");
+        $(".footer-link").children(1).eq().text("home");
+        $(".footer-link").children(1).eq().text("about");
+        $(".footer-link").children(1).eq().text("services");
+
+        $(".footer-link-contact").children(1).eq().text("contact");
+
+    });
+}
